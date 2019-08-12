7cbee3f3b43da68a6c70171df823bf0579e5334f
300943385 <300943385@PR-E216-41>
2019-08-12 15:14:00 -0400

    add master form and character generation form

--- FinalTestC/Program.cs
+++ FinalTestC/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FinalTestC.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,8 +7,13 @@ using System.Windows.Forms;
 
 namespace FinalTestC
 {
-    static class Program
+    public static class Program
     {
+
+
+        public static CharacterGeneration characterForm;
+
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -16,7 +22,10 @@ namespace FinalTestC
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            characterForm = new CharacterGeneration();
+
+            Application.Run(characterForm);
         }
     }
 }
---
+++ FinalTestC/Views/CharacterGeneration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FinalTestC.Views
+{
+    public partial class CharacterGeneration : FinalTestC.Views.MasterForm
+    {
+        public CharacterGeneration()
+        {
+            InitializeComponent();
+        }
+    }
+}
